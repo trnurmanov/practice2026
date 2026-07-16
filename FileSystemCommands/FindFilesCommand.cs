d85c6a80b4617364255c2efb0c7c031e8158a30a
trnurmanov <nurmanov020407@gmail.com>
2026-07-16 21:00:55 +0300

    task08

---
+++ FileSystemCommands/FindFilesCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using CommandLib;
+
+namespace FileSystemCommands
+{
+    public class FindFilesCommand : ICommand
+    {
+        private readonly string DirPath;
+        private readonly string SearchPattern;
+        public List<string> FoundFiles { get; private set; } = new List<string>();
+
+        public FindFilesCommand(string dirPath, string searchPattern)
+        {
+            DirPath = dirPath;
+            SearchPattern = searchPattern;
+        }
+
+        public void Execute()
+        {
+            if (!Directory.Exists(DirPath))
+            {
+                Console.WriteLine("Директория не найдена: " + DirPath);
+                return;
+            }
+            FoundFiles = Directory.GetFiles(DirPath, SearchPattern).ToList();
+
+            Console.WriteLine("Файлы по маске " + SearchPattern + ":");
+            foreach (var file in FoundFiles)
+            {
+                Console.WriteLine(Path.GetFileName(file));
+            }
+        }
+    }
+}
---
+++ task08tests/FileSystemCommandsTests.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using Xunit;
+using FileSystemCommands;
+
+namespace task08tests
+{
+    public class FileSystemCommandsTests
+    {
+        [Fact]
+        public void DirectorySizeCommand_ShouldCalculateSize()
+        {
+
+            var testDir = Path.Combine(Path.GetTempPath(), "TestDirSize");
+            if (Directory.Exists(testDir)) Directory.Delete(testDir, true);
+            Directory.CreateDirectory(testDir);
+            File.WriteAllText(Path.Combine(testDir, "test1.txt"), "Hello");
+            File.WriteAllText(Path.Combine(testDir, "test2.txt"), "World");
+            var command = new DirectorySizeCommand(testDir);
+            command.Execute();
+            Assert.Equal(10, command.CalculatedSize);
+            Directory.Delete(testDir, true);
+        }
+
+        [Fact]
+        public void FindFilesCommand_ShouldFindMatchingFiles()
+        {
+            var testDir = Path.Combine(Path.GetTempPath(), "TestDirFind");
+            Directory.CreateDirectory(testDir);
+            File.WriteAllText(Path.Combine(testDir, "file1.txt"), "Text");
+            File.WriteAllText(Path.Combine(testDir, "file2.log"), "Log");
+            var command = new FindFilesCommand(testDir, "*.txt");
+            command.Execute();
+            Assert.Single(command.FoundFiles);
+            Assert.Contains("file1.txt", command.FoundFiles[0]);
+            Directory.Delete(testDir, true);
+        }
+
+    }
+}
