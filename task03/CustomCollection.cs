faf4a62815c76082ebc3a7a4638aa115c6e92a8f
trnurmanov <nurmanov020407@gmail.com>
2026-07-16 20:18:38 +0300

    task03

---
+++ task03/CustomCollection.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task03
+{
+    public class CustomCollection<T> : IEnumerable<T>
+    {
+        private readonly List<T> _items = new();
+
+        public void Add(T item)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+
+            _items.Add(item);
+        }
+
+        public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public IEnumerable<T> GetReverseEnumerator()
+        {
+            for (int i = _items.Count - 1; i >= 0; i--)
+            {
+                yield return _items[i];
+            }
+        }
+
+        public IEnumerable<T> FilterAndSort(Func<T, bool> predicate, Func<T, IComparable> keySelector)
+        {
+            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+
+            if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
+
+            return _items.Where(predicate).OrderBy(keySelector);
+        }
+
+        public static IEnumerable<int> GenerateSequence(int start, int count)
+        {
+            if (count < 0)
+                throw new ArgumentOutOfRangeException(nameof(count));
+
+            for (int i = 0; i < count; i++)
+            {
+                yield return start + i;
+            }
+        }
+    }
+}
