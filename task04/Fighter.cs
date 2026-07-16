f8224dda66af002cd6ce2c990f0cd37c9d2f1c0c
trnurmanov <nurmanov020407@gmail.com>
2026-07-16 20:23:03 +0300

    task04

---
+++ task04/Fighter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task04
+{
+    public class Fighter : ISpaceship
+    {
+        public int Speed => 100;
+        public int FirePower => 50;
+
+        public void MoveForward()
+        {
+            return;
+        }
+        public void Rotate(int angle)
+        {
+            return;
+        }
+        public void Fire()
+        {
+            return;
+        }
+    }
+}
---
+++ task04tests/SpaceshipTests.cs
@@ -0,0 +1,48 @@
+﻿
+using task04;
+using Xunit;
+
+public class SpaceshipTests
+{
+    [Fact]
+    public void Cruiser_ShouldHaveCorrectStats()
+    {
+        ISpaceship cruiser = new Cruiser();
+
+        Assert.Equal(50, cruiser.Speed);
+
+        Assert.Equal(100, cruiser.FirePower);
+    }
+
+    [Fact]
+    public void Cruiser_ShouldBeFasterThanFighter()
+    {
+        var fighter = new Fighter();
+
+        var cruiser = new Cruiser();
+
+        Assert.True(cruiser.FirePower > fighter.FirePower);
+    }
+
+    [Fact]
+    public void Fighter_ShouldHaveCorrectStats()
+    {
+
+        ISpaceship fighter = new Fighter();
+
+        Assert.Equal(100, fighter.Speed);
+
+        Assert.Equal(50, fighter.FirePower);
+    }
+
+    [Fact]
+    public void Fighter_ShouldBeFasterThanCruiser()
+    {
+
+        var fighter = new Fighter();
+
+        var cruiser = new Cruiser();
+
+        Assert.True(fighter.Speed > cruiser.Speed);
+    }
+}
