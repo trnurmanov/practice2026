88e0019f744a1c7834130747e828a9e22734597c
trnurmanov <nurmanov020407@gmail.com>
2026-07-16 20:11:16 +0300

    task01

---
+++ task01/StringExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace task01
+{
+
+    public static class StringExtensions
+    {
+
+        public static bool IsPalindrome(this string input)
+        {
+
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+
+            var cleanedChars = input.Where(c => !char.IsPunctuation(c) && !char.IsWhiteSpace(c)).Select(char.ToLower).ToArray();
+
+            if (cleanedChars.Length == 0)
+            {
+                return false;
+            }
+            var reversedChars = cleanedChars.Reverse().ToArray();
+
+
+            return cleanedChars.SequenceEqual(reversedChars);
+        }
+    }
+}
